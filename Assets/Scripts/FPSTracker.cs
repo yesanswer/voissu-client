b392353b810b68ded1b9e0e448c797ce6e172791
mapmiddle <mapmake@naver.com>
2016-06-24 13:46:40 +0900

    FPS 표시 라벨 추가

---
+++ Assets/Scripts/FPSTracker.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class FPSTracker : MonoBehaviour {
+
+    Text text;
+
+	// Use this for initialization
+	void Start () {
+        this.text = this.GetComponent<Text>();
+        StartCoroutine(UpdateFPS());
+	}
+
+    IEnumerator UpdateFPS() {
+        while(true) {
+            int fps = (int)(1.0f / Time.smoothDeltaTime);
+            text.text = string.Format("FPS : {0}", fps);
+            yield return new WaitForSeconds(1.0f);
+        }
+    }
+
+}
